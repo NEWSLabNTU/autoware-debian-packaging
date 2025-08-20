d0ace36a0c3e5a12f51e242b52f36a472bfe6c45
aeon <jerry73204@gmail.com>
2025-08-20 22:23:57 +0800

    Add notes in create-rosdep-list.sh about `rosdep update`

--- helper/create-rosdep-list.sh
+++ helper/create-rosdep-list.sh
@@ -18,6 +18,9 @@ ${pkg_name}:
 	done > "$rosdep_yaml_file"
 
     sudo sh -c "echo 'yaml file://${rosdep_yaml_file}' > '$rosdep_list_file'"
+    # We need to update rosdep to load the newly created custom package list
+    # This is required for bloom-generate to work correctly
+    echo "info: updating rosdep to load custom package list"
     rosdep update
 else
     echo 'info: skip generating rosdep list'
